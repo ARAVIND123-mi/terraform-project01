aae578a96253cb75383fdb8c24d16f22329659ff
ARAVIND123-mi <charavind10dulkar@gmail.com>
2024-10-17 12:19:33 +0530

    Update package.sh

--- package.sh
+++ package.sh
@@ -3,7 +3,7 @@ sudo yum install -y httpd
 sudo systemctl start httpd
 sudo systemctl enable httpd
 sudo yum install -y git
-git clone "https://github.com/ARAVIND123-mi/Mario.git"
+git clone "https://github.com/ARAVIND123-mi/ecomm.git"
 cd /
-sudo mv Mario/* /var/www/html/
+sudo mv ecomm/* /var/www/html/
 
